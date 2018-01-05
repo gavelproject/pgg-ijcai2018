f77d480761135d5e46757c4652f11ad75c9cf9f5
Igor C. A. de Lima <igorcadelima@gmail.com>
2018-01-04 22:51:09 -0600

    Fix calculation

--- src/agt/jia/exp.java
+++ src/agt/jia/exp.java
@@ -10,6 +10,14 @@ import jason.asSyntax.*;
  *
  */
 public class exp extends DefaultInternalAction {
+  private static InternalAction singleton = null;
+
+  public final static InternalAction create() {
+    if (singleton == null)
+      singleton = new exp();
+    return singleton;
+  }
+
   @Override
   public int getMinArgs() {
     return 1;
@@ -19,23 +27,24 @@ public class exp extends DefaultInternalAction {
   public int getMaxArgs() {
     return 2;
   }
-  
+
   @Override
   protected void checkArguments(Term[] args) throws JasonException {
     super.checkArguments(args);
     if (!args[0].isNumeric()) {
-      throw new JasonException(
-          "The first argument of the internal action " + getClass().getCanonicalName() + " is not a number.");
+      throw new JasonException("The first argument of the internal action "
+          + getClass().getCanonicalName() + " is not a number.");
     } else if (!args[1].isVar()) {
-      throw new JasonException(
-          "The second argument of the internal action " + getClass().getCanonicalName() + " is not a variable.");
+      throw new JasonException("The second argument of the internal action "
+          + getClass().getCanonicalName() + " is not a variable.");
     }
-  } 
+  }
 
   @Override
   public Object execute(TransitionSystem ts, Unifier un, Term[] args) throws Exception {
+    checkArguments(args);
     double power = ((NumberTerm) args[0]).solve();
-    final NumberTerm result = ASSyntax.createNumber(power);
+    final NumberTerm result = ASSyntax.createNumber(Math.exp(power));
     return un.unifies(result, args[1]);
   }
 }
